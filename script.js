9968a4008c41d13bb4238671cbb51b66d683ab3f
y4sh1k4 <mehndirattayashika5@gmail.com>
2023-09-09 11:33:05 +0530

    loginpage_2

---
+++ script.js
@@ -0,0 +1,20 @@
+const wrapper=document.querySelector('.wrapper');
+const register=document.querySelector('.register_');
+const login=document.querySelector('.login_');
+const button=document.querySelector('.botn');
+const close=document.querySelector('.icon-close');
+
+register.onclick=function(event){
+    event.preventDefault();
+    wrapper.classList.add('active');
+};
+login.onclick=function(event){
+    event.preventDefault();
+    wrapper.classList.remove('active');
+};
+button.onclick=function(){
+    wrapper.classList.add('button-active');
+};
+close.onclick=function(){
+    wrapper.classList.remove('button-active');
+};
