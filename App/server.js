4f797f0d4e02c8641dcf96b1085b5b4bad4530e3
sh4d0wy <sakshambhugra8@gmail.com>
2023-09-09 10:05:08 +0530

    serverside rendering #2

--- App/server.js
+++ App/server.js
@@ -7,16 +7,17 @@ const loginRouter = require('./router/login');
 const userData = require('./router/data');
 const path = require('path');
 
+app.use(express.static('public'))
 app.use(express.urlencoded({extended:true}))
 app.use(express.json());
-app.use(express.static('./frontendcode/html'))
 app.use('/signup',signupRouter)
 app.use('/login',loginRouter)
 app.use('/data',userData)
 
 app.get('/',(req,res)=>{
-    res.sendFile(__dirname+'/frontencode/html/index.html')
+    res.sendFile(__dirname,'/public/index.html')
 })
+
 app.listen(3000,()=>{
     console.log('server started')
 })
